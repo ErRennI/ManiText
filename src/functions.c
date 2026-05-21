c66fb93a52dfc1d5be9de72c5b3ac28515e0e85f
Miray_Duygulu <84855380+Miray-01@users.noreply.github.com>
2026-05-21 21:03:23 +0300

    Implement print and insert functions in functions.c
    Added print and insert functions for managing text buffer.

--- src/functions.c
+++ src/functions.c
@@ -11,3 +11,65 @@ void edit(char* filename){
 void save();
 
 //Fonksiyonlarınızı burdan sonrasına ekleyin insert delete gibi
+
+// 20 Mayıs 2026
+//********************
+void print() {
+    int current = head; 
+    int screen_line = 0; 
+
+    if (current == -1) {
+        // check if the text buffer is empty
+        mvprintw(0, 0, "Buffer is empty. Please press E to open file.");
+        return;
+    }
+
+    while (current != -1) {
+        // Loop through the buffer following the links until the end
+        mvprintw(screen_line, 0, "%s", textbuffer[current].statement);
+        screen_line++;
+        current = textbuffer[current].next; 
+    }
+}
+
+// 20 Mayıs 2026
+//********************
+//garbageCollection()
+//new_node_index
+//free_index
+//next_node_index
+//noecho araştır
+void insert(int index) {
+    if (free_index >= 100 || free_index == -1) {
+        //otomatik GarbageCollection
+        garbageCollection();
+        if (free_index == -1) return;
+    }
+
+    int new_node_index = free_index; // yeni veriyi koyacağımız indeks
+
+    echo(); 
+    mvprintw(LINES - 1, 0, "New line: ");
+    getnstr(textbuffer[new_node_index].statement, 39); // max 39 char
+    noecho(); // tekrar gizli moda geç 
+
+   
+    
+    int next_node_index = textbuffer[index].next;
+    textbuffer[new_node_index].prev = index;           
+    textbuffer[new_node_index].next = next_node_index;  
+     // Örnek: A -> B arasına YENI ekliyoruz (A = index, YENI = new_node_index)
+
+    
+    textbuffer[index].next = new_node_index;           
+
+    // Eğer ekleme listenin en sonuna yapılmıyorsa, B'nin arkasını yeni elemana bağla
+    if (next_node_index != -1) {
+        textbuffer[next_node_index].prev = new_node_index; 
+    } else {
+        tail = new_node_index;    // if it is end then it is tail now
+    }
+    
+    free_index++; //increase
+}
+
